68cd1228e1aad2fddf374a9de4f549565bca631f
lucasbiazonc <lucas.crosatti@escola.pr.gov.br>
2026-05-12 11:58:22 -0300

    começando a 3 de listas

--- index.js
+++ index.js
@@ -1,3 +1,29 @@
-// run `node index.js` in the terminal
 
-console.log(`Hello Node.js v${process.versions.node}!`);
+console.log (LISTAS)
+// 1 atividade do slide
+console.log('Atividade 1')
+let frutas = ['Maçã','Pera','Banana','Laranja','Abacate'];
+console.log (frutas[0], frutas[1], frutas[2], frutas[3], frutas[4]);
+console.log ();
+console.log (frutas[0]);
+console.log ();
+console.log(frutas[4]);
+
+//espaçamento
+console.log();
+console.log();
+
+//2 atividade do slide
+console.log('Atividade 2');
+let at2 = [2, 7, 9, 6, 12, 55];
+let soma = 0;
+
+for (let calc = 0; calc < at2.length; calc++) {
+    soma = soma + at2[calc];
+};
+console.log(soma)   
+
+console.log()
+console.log()
+//att 3
+console.log('Atividade 3')
